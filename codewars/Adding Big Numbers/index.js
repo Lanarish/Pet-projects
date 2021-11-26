d5619a7af6615de844166767455be646dffa4610
Lanarish <lanarish@gmail.com>
2021-11-26 14:46:14 +0300

    Added solution

--- codewars/Adding Big Numbers/index.js
+++ codewars/Adding Big Numbers/index.js
@@ -9,7 +9,31 @@
 // The input numbers are big.
 // The input is a string of only digits
 // The numbers are positives
-function add(a, b) {
-  return (Number(a) + Number(b)).toString();
+function add(A, B) {
+  const AL = A.length;
+  const BL = B.length;
+  const ML = Math.max(AL, BL);
+  console.log(AL, BL, ML);
+  let carry = 0,
+    sum = "";
+
+  for (let i = 1; i <= ML; i++) {
+    let a = +A.charAt(AL - i);
+    console.log("a ", a);
+    let b = +B.charAt(BL - i);
+    console.log("b ", b);
+
+    let t = carry + a + b;
+    console.log("t ", t);
+    carry = (t / 10) | 0;
+    console.log("carry ", carry);
+    t %= 10;
+    console.log("t1 ", t);
+
+    sum = i === ML && carry ? carry * 10 + t + sum : t + sum;
+  }
+
+  return sum;
 }
-console.log(add("100", "100"));
+
+console.log(add("3874", "94089"));
