501b333808d0819fd84b81bfd0b2d322d1b761db
Lanarish <lanarish@gmail.com>
2021-11-29 10:20:38 +0300

    Added solution for Flatten_a_Nested_Map task

---
+++ codewars/Flatten a Nested Map/index.js
@@ -0,0 +1,18 @@
+function flattenMap(map) {
+  let to_return = {};
+  for (let k of Object.keys(map)) {
+    if (map[k] && typeof map[k] === "object" && !Array.isArray(map[k])) {
+      let flatObj = flattenMap(map[k]);
+      for (let i in flatObj) {
+        to_return[k + "/" + i] = flatObj[i];
+      }
+    } else {
+      if (map[k]) {
+        to_return[k] = map[k];
+      } else {
+        to_return[k] = null;
+      }
+    }
+  }
+  return to_return;
+}
