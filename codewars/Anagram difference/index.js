f1cd9c43d55641bfd0a958e9da5eb26f5f7ce7bc
Lanarish <lanarish@gmail.com>
2021-11-29 18:06:35 +0300

    Added changes according to mentor's comment

--- codewars/Anagram difference/index.js
+++ codewars/Anagram difference/index.js
@@ -3,8 +3,9 @@ function anagramDifference(w1, w2) {
   let w2Arr = w2.split("");
 
   for (let i = 0; i < w1Arr.length; ++i) {
-    if (w2Arr.indexOf(w1Arr[i]) != -1) {
-      w2Arr.splice(w2Arr.indexOf(w1Arr[i]), 1);
+    const w2Index = w2Arr.indexOf(w1Arr[i]);
+    if (w2Index != -1) {
+      w2Arr.splice(w2Index, 1);
       w1Arr.splice(i, 1);
       i--;
     }
