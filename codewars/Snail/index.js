52abc6e5924a8ec5a220765da9b2079c2e032b9a
Lanarish <lanarish@gmail.com>
2021-12-01 13:04:56 +0300

    Added comment accordig to comments

--- codewars/Snail/index.js
+++ codewars/Snail/index.js
@@ -1,17 +1,22 @@
-const snail = (array) => {
-  let result = [];
-  while (array.length) {
-    result.push(...array.shift());
-    for (let i = 0; i < array.length; i++) {
-      result.push(array[i].pop());
-    }
-    result.push(...(array.pop() || []).reverse());
-    for (let i = array.length - 1; i >= 0; i--) {
-      result.push(array[i].shift());
-    }
-  }
+const snail = function(arr) {
+  const result = [];
+  let top = 0, bottom = arr.length-1;
+  let left = 0, right = arr[0].length-1;
+  
+  do {
+    for (let i = left; i <= right; i++)
+    {result.push(arr[top][i])} // top row
+    for (let i = top+1; i <= bottom; i++)
+    {result.push(arr[i][right])} // right column
+    for (let i = right-1; i >= left; i--)
+    {result.push(arr[bottom][i])} // bottom row
+    for (let i = bottom-1; i > top; i--)
+    {result.push(arr[i][left])} // left column
+    top++; bottom--; left++; right--;
+  } while (top <= bottom);
+  
   return result;
-};
+}
 
 console.log(
   snail([
