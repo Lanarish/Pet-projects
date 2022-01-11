4dbec04a18cb10e63169411b8ead40569671d4cb
Lanarish <lanarish@gmail.com>
2022-01-11 10:20:07 +0300

    added test fo duel class

--- rpgsaga/tests/Duel.spec.ts
+++ rpgsaga/tests/Duel.spec.ts
@@ -1,5 +1,6 @@
 import { Duel } from '../src/Duel';
 import { Archer } from '../src/Heroes/Archer';
+import { Wizard } from '../src/Heroes/Wizard';
 import { HeroPairs } from '../src/HeroPairs';
 import { Logger } from '../src/Logger';
 
@@ -11,7 +12,7 @@ let duel;
 beforeEach(() => {
   logger = new Logger();
   firstHero = new Archer('Archer', 'Sam', 'Jonson', 34, 80, logger);
-  secondHero = new Archer('Archer', 'Sam', 'Jonson', 34, 80, logger);
+  secondHero = new Wizard('Wizard', 'Sam', 'Jonson', 34, 80, logger);
   heroPair = new HeroPairs(firstHero, secondHero);
   duel = new Duel(heroPair, logger);
 });
@@ -20,5 +21,20 @@ describe('whoIsFirst function', () => {
   it('return boolean value', () => {
     const result = typeof duel.whoIsFirst();
     expect(result).toEqual('boolean');
+  });
+});
+describe('attackChecker function:', () => {
+  it('check that Wizard superPowerJustNow is false', () => {
+    duel.attackChecker(firstHero, secondHero);
+    expect(secondHero.superPower.SuperPowerJustNow).toBeFalsy();
+  });
+  it('check that after use superPower the superPowerJustNow is true', () => {
+    secondHero.superPower.useSuperPower(secondHero);
+    expect(secondHero.superPower.SuperPowerJustNow).toBeTruthy();
+  });
+  it('check that after use onse superPower the superPowerJustNow is false again', () => {
+    secondHero.superPower.useSuperPower(secondHero);
+    duel.attackChecker(firstHero, secondHero);
+    expect(secondHero.superPower.SuperPowerJustNow).toBeFalsy();
   });
 });
