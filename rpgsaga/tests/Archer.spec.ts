7317e8a4fe540832e323773ee7dd7be41c634ef6
Lanarish <lanarish@gmail.com>
2022-01-17 15:50:19 +0300

    .

--- rpgsaga/tests/Archer.spec.ts
+++ rpgsaga/tests/Archer.spec.ts
@@ -1,23 +1,41 @@
 import { Logger } from '../src/Logger';
 import { FireArrows } from '../src/Super_Powers/FireArrows';
 import { Archer } from '../src/Heroes/Archer';
-
+import { Duel } from '../src/Duel';
+const ADD_DAMAGE_TEST = 2;
 describe('Check Archer :', () => {
   let logger;
-  let archer;
+  let attacker;
+  let opponent;
   let superpower;
+
   beforeEach(() => {
     logger = new Logger();
-    archer = new Archer('Archer', 'Sam', 'Jonson', 34, 80, logger);
+    attacker = new Archer('Archer', 'Sam', 'Jonson', 34, 80, logger);
+    opponent = new Archer('Archer', 'Tom', 'Jackson', 14, 90, logger);
     superpower = new FireArrows(logger);
+    duel = new Duel();
+
   });
   it('status is false before each start', () => {
     expect(superpower.SuperPowerInRoundStatus).toBeFalsy();
     expect(superpower.SuperPowerJustNow).toBeFalsy();
   });
   it('change status after used superpower ', () => {
-    archer.superPower.useSuperPower(archer, archer);
-    expect(archer.superPower.SuperPowerInRoundStatus).toBeTruthy();
-    expect(archer.superPower.SuperPowerJustNow).toBeTruthy();
+    attacker.superPower.useSuperPower(attacker, opponent);
+    expect(attacker.superPower.SuperPowerInRoundStatus).toBeTruthy();
+    expect(attacker.superPower.SuperPowerJustNow).toBeTruthy();
+  });
+
+  it('increace his damage in 2 points if he use SuperPower', () => {
+   
+let oldHealth = opponent.Health;
+      attacker.superPower.useSuperPower(attacker, opponent);
+    duel.attackChecker(attacker,opponent);
+      expect(opponent.Health).toEqual(oldHealth - (attacker.Power+ADD_DAMAGE_TEST));
+      oldHealth = opponent.Health;
+      duel.attackChecker(attacker,opponent);
+      expect(opponent.Health).toEqual(oldHealth - (attacker.Power+ADD_DAMAGE_TEST));
+    }
   });
 });
